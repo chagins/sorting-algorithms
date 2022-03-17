3610fb7864207f769595ae3acb139063dce57fec
chagins <chagin_yav@hotmail.com>
2022-03-17 21:41:25 +0500

    feat: add selectionsort function with test

---
+++ test/test_SelectionSort.js
@@ -0,0 +1,16 @@
+const assert = require("assert");
+const shuffled_arrays = require('../test/shuffled_arrays.js');
+const shuffledArrs = shuffled_arrays.shuffledArrs;
+const orderedArr = shuffled_arrays.orderedArr;
+
+//sorting function
+const SelectionSort = require('../src/SelectionSort.js');
+
+describe(`${SelectionSort.name}`, () => {
+  for(let i = 0; i < shuffledArrs.length; i++){
+    it("Should return 'ordered array with numbers from 1 to 100'", () => {
+      const testArr = SelectionSort(shuffledArrs[i]);
+      assert.deepEqual(testArr, orderedArr);
+    })
+  }
+})
